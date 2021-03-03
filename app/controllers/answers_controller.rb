4db26e5d189abc06f7485c20ad5580f4e7a3c728
kpkodil <kpkopb@gmail.com>
2021-03-04 00:41:17 +0300

    add specs for answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,25 @@
 class AnswersController < ApplicationController
+
+  def new; end
+
+  def create
+    @answer = question.answers.build(answer_params)
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question
+    @question ||= Question.find(params[:question_id])
+  end
+
+  helper_method :question
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,4 +2,55 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
 
+  let(:question) { create(:question) }
+
+  describe 'GET #new' do
+
+    it 'renders new view' do
+      get :new, params: { question_id: question }
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    
+    it 'associated with question' do
+      answer =  create(:answer, question: question)
+      expect(question).to eq answer.question
+    end
+
+    context 'with valid attributes' do
+
+      it 'saves a new answer in the database' do
+        expect { post :create, params: valid_answer_params }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: valid_answer_params
+        expect(response).to redirect_to assigns(:answer)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect { post :create, params: invalid_answer_params }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: invalid_answer_params
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  private 
+
+  def valid_answer_params
+    { answer: attributes_for(:answer), question_id: question }
+  end
+
+  def invalid_answer_params
+    { answer: attributes_for(:answer, :invalid), question_id: question }
+  end
+
 end
