6d9ed7a8b3d1fc6321521799907b8de9ef109882
kpkodil <kpkopb@gmail.com>
2021-03-04 16:31:15 +0300

    fix saves a new answer in the database answers controller spec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe AnswersController, type: :controller do
     context 'with valid attributes' do
 
       it 'saves a new answer in the database' do
-        expect { post :create, params: valid_answer_params }.to change(Answer, :count).by(1)
+        expect { post :create, params: valid_answer_params }.to change(question.answers, :count).by(1)
       end
 
       it 'redirects to show view' do
