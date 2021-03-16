277191132d4ea25b6e9307eaca1b8a9861a40e2f
kpkodil <kpkopb@gmail.com>
2021-03-16 13:18:50 +0300

    add 'asks a question with attached files' question feature spec

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_one :best_answer, -> { where(best: true ) }, class_name: 'Answer'
 
-  has_one_attached :file
+  has_many_attached :files
   
   validates :title, :body, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Question, type: :model do
     expect(question.best_answer).to eq answer
   end
 
-  it 'have one attached file' do
-    expect(Question.new.file).to be_an_instance_of(ActiveStorage::Attached::One)
+  it 'have many attached files' do
+    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
 end
