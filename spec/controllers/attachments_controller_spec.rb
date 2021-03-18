d0bf4e0f937562a9f988b87e04c164430f0de094
kpkodil <kpkopb@gmail.com>
2021-03-19 00:27:51 +0300

    fix attachments_controller_spec.rb

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe AttachmentsController, type: :controller do
   let(:user) { create(:user) }
   let(:author) { create(:user) }
-  let(:question) { create(:question, user: user) }
+  let(:question) { create(:question, user: author) }
 
   describe 'DELETE#delete_attached_file' do
 
