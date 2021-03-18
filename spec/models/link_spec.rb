f6b19e45c602f2380142706095b3f780565b80ea
kpkodil <kpkopb@gmail.com>
2021-03-18 21:05:44 +0300

    add validation of link url format

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+  it { should_not allow_value('invalid').for :url }
+  it { should allow_value('http://valid.com').for :url }
 end
