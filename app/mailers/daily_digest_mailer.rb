fe758ab2e42faca23e89a967b6d4cae8c41ebf05
kpkodil <kpkopb@gmail.com>
2021-04-16 00:51:02 +0300

    add tmeplate to  DaliyDigestMailer

--- app/mailers/daily_digest_mailer.rb
+++ app/mailers/daily_digest_mailer.rb
@@ -2,6 +2,7 @@ class DailyDigestMailer < ApplicationMailer
   def digest(user, questions)
     @today_questions = questions
 
-    mail to: user.email
+    mail to: user.email,
+         template_path: 'mailers/daily_digest'
   end
 end
